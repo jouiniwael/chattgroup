e53ea04f4d1b4775621fb58663c6a61001808850
unknown <you@example.com>
2021-05-23 23:16:01 +0200

    done

--- server.js
+++ server.js
@@ -1,69 +1,71 @@
-const express= require ('express')  
-const http =require ("http")
-const app = express ();
-const server =http.createServer(app)
-const socket =require ('socket.io')
-const io =  socket (server);
-    
-let users =[]
-const messages ={
-    general :[],
-    random:[],
-    jokes :[],
-    javascript :[],
-}
+const express = require("express");
+const http = require("http");
+const app = express();
+const cors = require("cors");
+app.use(cors("*"));
+const server = http.createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["PUT", "GET", "POST", "DELETE", "OPTIONS"],
+    credentials: true,
+  },
+});
+
+let users = [];
+const messages = {
+  general:[],
+  
+};
 
 
-io.on ("connection",  socket =>{
-    socket.on("join server" , (username)=>{
-        const user ={
-            username,
-            id:socket.id ,
-        };
-        users.push(user);
-        io.emit("new user", users );
-    } )
-    socket.on ("join room ", (roomName,cb)=>{
-        socket.join(roomName);
-        cb(messages[roomName]);
-    })
-    
-    socket.on("send message", ({content, to, sender,chatName , isChannel})=>{
-        if (isChannel){
-            const payload ={
-                content,
-                chatName,
-                sender,
-            };
-            socket.to(to).emit("new message", payload);
+io.on("connection", (socket) => {
+  socket.on("join server", (username) => {
+    const user = {
+      username,
+      id: socket.id,
+    };
+    users.push(user);
+    io.emit("new user", users);
+  });
+  socket.on("join room", (roomName, cb) => {
+    socket.join(roomName);
+    cb(messages[roomName]);
+  });
+  
+  socket.on("add room", (room) => {
+      const result={...messages, room}
+    io.emit("getNewRoom",result );
+     console.log( "newwww" ,result)
+  });
 
-        } else {
-            const   payload ={
-                content, 
-                chatName: sender,
-                sender ,
-            }
-            socket.to(to).emit("new message", payload);
-        }
-        if (messages[chatName]) {
-            messages[chatName].push({
-                sender ,
-                content
-            })
-        }
-    });
-    socket.on("disconnect", ()=>{
-        users=users.filter(u=>u.id!==socket.id )
-        io.emit("new user",users)
-    })
+  socket.on("send message", ({ content, to, sender, chatName, isChannel }) => {
+    if (isChannel) {
+      const payload = {
+        content,
+        chatName,
+        sender,
+      };
+      io.to(to).emit("backmessage", payload);
+    } else {
+      const payload = {
+        content,
+        chatName: sender,
+        sender,
+      };
+      io.to(to).emit("backmessage", payload);
+    }
+    if (messages[chatName]) {
+      messages[chatName].push({
+        sender,
+        content,
+      });
+    }
+  });
+  socket.on("disconnect", () => {
+    users = users.filter((u) => u.id !== socket.id);
+    io.emit("new user", users);
+  });
+});
 
-
-})
-
-
-
-
-
-
-
-server.listen(1337, ()=>console.log('server is listening on port1337'))
+server.listen(1337, () => console.log("server is listening on port1337"));
